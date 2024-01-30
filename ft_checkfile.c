94dadcf25ddbf16c532750ab637ce53ce60068e8
CapSte42 <147485466+CapSte42@users.noreply.github.com>
2024-01-30 16:22:30 +0100

    Add files via upload

--- ft_checkfile.c
+++ ft_checkfile.c
@@ -6,7 +6,7 @@
 /*   By: smontuor <smontuor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:35:53 by smontuor          #+#    #+#             */
-/*   Updated: 2024/01/25 17:48:16 by smontuor         ###   ########.fr       */
+/*   Updated: 2024/01/27 19:58:24 by smontuor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,58 @@ static void	is_fdf_file(const char *file)
 
 }
 
-static void	check_consistency(int n_lines, char **all_lines)
+static int	count_elements(const char *line)
 {
-	int	i;
-	char	**line;
+	int count;
+	int in_element;
+	int i;
 
-	i = 0;
-	while (i < n_lines)
+	if (line == NULL)
 	{
-		ft_printf(2, "<%s>\n",all_lines[i]);
-		line = ft_split(all_lines[i], ' ');
-		if (check_line(line) /*AAAAAAAAAAAAAAAAAAAAAAAAAAAAA*/)
-			/* AAAAAAAAAAAAAAAAAA */
+		return (-1);
+	}
+	count = 0;
+	in_element = 0;
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] != ' ' && in_element == 0)
+		{
+			in_element = 1;
+			count++;
+        } else if (line[i] == ' ')
+		{
+			in_element = 0;
+		}
 		i++;
 	}
+    return (count);
 }
 
-static int	check_line(char **line)
+static void check_consistency(int n_lines, char **all_lines)
 {
-	
+	int	i;
+	int	expected_elements;
+	int	num_elements;
+
+	if (n_lines <= 0 || all_lines == NULL)
+		// Gestisci caso di input non valido
+	i = 0;
+	expected_elements = count_elements(all_lines[0]);
+	if (expected_elements == -1)
+		// Gestisci l'errore o la riga vuota
+	i = 1;
+	while (i < n_lines)
+	{
+		num_elements = count_elements(all_lines[i]);
+		if (num_elements != expected_elements)
+		{
+			// Inconsistenza trovata, gestisci come necessario
+			return;
+		}
+		i++;
+	}
+    // Tutte le righe sono consistenti
 }
 
 void	ft_checkfile(char *file)
@@ -51,12 +84,13 @@ void	ft_checkfile(char *file)
 	int		n_lines;
 
 	if (!file)
-		ft_exit_error("Please enter a .fdf file as an argument.");
+		ft_exit_error("Yeah. Whatever.");
 	is_fdf_file(file);
 	n_lines = ft_get_all_lines(file, &all_lines);
 	if (n_lines == -1)
-		ft_exit_error("File passed as argument is empty.");
-	/* check_consistency(n_lines, all_lines); */
+		ft_exit_error("File passed as argument is dumb.");
+	check_consistency(n_lines, all_lines);
+	
 }
 
 /* devo controllare la coerenza della matrice ottenuta, ossia se e' compatibile con
